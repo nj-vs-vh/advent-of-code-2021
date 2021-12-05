23c40961c9b302dc546288241ccd401387378c5d
Igor Vaiman <gosha.vaiman@gmail.com>
2021-12-05 13:54:03 +0300

    day5 solved

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,27 @@ mod day01;
 mod day02;
 mod day03;
 mod day04;
+mod day05;
 
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1 {
-        let day_to_run: u8 = (&args[1]).parse().expect("Command line argument must be an integer");
+        let day_to_run: u8 = (&args[1])
+            .parse()
+            .expect("Command line argument must be an integer");
         match day_to_run {
-            1 => {day01::sonar();}
-            2 => {day02::submarine_path()}
-            3 => {day03::submarine_ratings()}
-            _ => {println!("Day {} is not yet implemented", day_to_run)}
+            1 => day01::sonar(),
+            2 => day02::submarine_path(),
+            3 => day03::submarine_ratings(),
+            4 => day04::squid(),
+            5 => day05::vent_lines(),
+            _ => {
+                println!("Day {} is not yet implemented", day_to_run)
+            }
         }
-    }
-    else {
-        day04::squid()
+    } else {
+        // new day will be here...
     }
 }
